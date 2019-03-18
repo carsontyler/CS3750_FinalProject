4f3eef612a21a76ddbc8241ffb0a0c582c797c6d
Tyler Davies <tyspace01@gmail.com>
2019-03-18 10:51:54 -0600

    added a button and click event to open file dialog

--- CS3750_FinalProject/MainWindow.xaml.cs
+++ CS3750_FinalProject/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace CS3750_FinalProject
 {
@@ -23,6 +24,23 @@ namespace CS3750_FinalProject
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var filedialog = new OpenFileDialog();
+            if(filedialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    string file = filedialog.FileName;
+
+                }
+                catch (Exception Ex)
+                {
+                    Console.Write(Ex);
+                }
+            }
         }
     }
 }
