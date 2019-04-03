85a078fdb28bf32b1f1eb4c40f4b3a1967eb9eaa
carsontyler <17ctyler@gmail.com>
2019-04-03 11:07:40 -0600

    Added splash screen on start up, various UI updates, removed Excel nuget package.

--- CS3750_FinalProject/MainWindow.xaml.cs
+++ CS3750_FinalProject/MainWindow.xaml.cs
@@ -27,6 +27,13 @@ namespace CS3750_FinalProject
 
         #region Constructor
 
+        public MainWindow(string fileName = "")
+        {
+            InitializeComponent();
+            Colleges = new List<College>();
+            if (!string.IsNullOrEmpty(fileName))
+                ParseFile(fileName);
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -55,53 +62,61 @@ namespace CS3750_FinalProject
             if (filedialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 string file = filedialog.FileName;
-                TextFieldParser parser = new TextFieldParser(file) { HasFieldsEnclosedInQuotes = true };
-                parser.SetDelimiters(",");
-
-                List<string> headers = new List<string>();
-
-                while (!parser.EndOfData)
-                {
-                    var fields = parser.ReadFields();
-                    if (fields[0] == "")
-                        continue;
-
-                    if (fields[0] == "CLG" || fields[0] == "ID")
-                    {
-                        foreach (var field in fields)
-                            headers.Add(field);
-                        continue;
-                    }
-
-                    var employee = new Employee
-                    {
-                        College = fields[headers.IndexOf("CLG")],
-                        Department = fields[headers.IndexOf("DEPT.")],
-                        Name = fields[headers.IndexOf("NAME")],
-                        Rank = fields[headers.IndexOf("RNK")],
-                        SalaryAmount = int.Parse(fields[headers.IndexOf("9MSALARY")])
-                    };
-
-                    if (!Colleges.Any(a => a.CollegeName == fields[headers.IndexOf("CLG")]))
-                        Colleges.Add(new College(fields[headers.IndexOf("CLG")]));
-
-                    if (!Colleges.FirstOrDefault(a => a.CollegeName == fields[headers.IndexOf("CLG")]).
-                            Departments.Any(a => a.DepartmentName == fields[headers.IndexOf("DEPT.")]))
-                        Colleges.FirstOrDefault(a => a.CollegeName == fields[headers.IndexOf("CLG")]).
-                            Departments.Add(new Department(fields[headers.IndexOf("DEPT.")]));
-
-                    Colleges.FirstOrDefault(a => a.CollegeName == fields[headers.IndexOf("CLG")]).
-                        Departments.First(a => a.DepartmentName == fields[headers.IndexOf("DEPT.")]).Employees.Add(employee);
-                }
-
-                InversionCalculator.CalcInversion(Colleges);
-                
-                DataGridCollege.ItemsSource = Colleges;
-                HomeScreen.Visibility = Visibility.Hidden;
-                InversionDataView.Visibility = Visibility.Visible;
+                ParseFile(file);
             }
         }
 
+        private void ParseFile(string file)
+        {
+            BrushConverter brush = new BrushConverter();
+            DataGridButton.Background = (Brush)brush.ConvertFrom("#bb33ff");
+            DataGridButton.Foreground = new SolidColorBrush(Colors.White);
+            TextFieldParser parser = new TextFieldParser(file) { HasFieldsEnclosedInQuotes = true };
+            parser.SetDelimiters(",");
+
+            List<string> headers = new List<string>();
+
+            while (!parser.EndOfData)
+            {
+                var fields = parser.ReadFields();
+                if (fields[0] == "")
+                    continue;
+
+                if (fields[0] == "CLG" || fields[0] == "ID")
+                {
+                    foreach (var field in fields)
+                        headers.Add(field);
+                    continue;
+                }
+
+                var employee = new Employee
+                {
+                    College = fields[headers.IndexOf("CLG")],
+                    Department = fields[headers.IndexOf("DEPT.")],
+                    Name = fields[headers.IndexOf("NAME")],
+                    Rank = fields[headers.IndexOf("RNK")],
+                    SalaryAmount = int.Parse(fields[headers.IndexOf("9MSALARY")])
+                };
+
+                if (!Colleges.Any(a => a.CollegeName == fields[headers.IndexOf("CLG")]))
+                    Colleges.Add(new College(fields[headers.IndexOf("CLG")]));
+
+                if (!Colleges.FirstOrDefault(a => a.CollegeName == fields[headers.IndexOf("CLG")]).
+                        Departments.Any(a => a.DepartmentName == fields[headers.IndexOf("DEPT.")]))
+                    Colleges.FirstOrDefault(a => a.CollegeName == fields[headers.IndexOf("CLG")]).
+                        Departments.Add(new Department(fields[headers.IndexOf("DEPT.")]));
+
+                Colleges.FirstOrDefault(a => a.CollegeName == fields[headers.IndexOf("CLG")]).
+                    Departments.First(a => a.DepartmentName == fields[headers.IndexOf("DEPT.")]).Employees.Add(employee);
+            }
+
+            InversionCalculator.CalcInversion(Colleges);
+
+            DataGridCollege.ItemsSource = Colleges;
+            HomeScreen.Visibility = Visibility.Hidden;
+            InversionDataView.Visibility = Visibility.Visible;
+        }
+        
         private void ExpandRow(object sender, RoutedEventArgs e)
         {
             DependencyObject obj = (DependencyObject)e.OriginalSource;
@@ -182,6 +197,12 @@ namespace CS3750_FinalProject
 
         private void ShowDataGrid(object sender, RoutedEventArgs e)
         {
+            Brush ogColor = DataGridButton.Background;
+            SummaryButton.Background = ogColor;
+            BrushConverter brush = new BrushConverter();
+            DataGridButton.Background = (Brush)brush.ConvertFrom("#bb33ff");
+            DataGridButton.Foreground = new SolidColorBrush(Colors.White);
+            SummaryButton.Foreground = new SolidColorBrush(Colors.Black);
             HomeScreen.Visibility = Visibility.Hidden;
             InversionDataView.Visibility = Visibility.Visible;
             SummaryView.Visibility = Visibility.Collapsed;
@@ -189,6 +210,12 @@ namespace CS3750_FinalProject
 
         private void ShowSummary(object sender, RoutedEventArgs e)
         {
+            Brush ogColor = SummaryButton.Background;
+            DataGridButton.Background = ogColor;
+            BrushConverter brush = new BrushConverter();
+            SummaryButton.Background = (Brush)brush.ConvertFrom("#bb33ff");
+            DataGridButton.Foreground = new SolidColorBrush(Colors.Black);
+            SummaryButton.Foreground = new SolidColorBrush(Colors.White);
             HomeScreen.Visibility = Visibility.Hidden;
             SummaryView.Visibility = Visibility.Visible;
             InversionDataView.Visibility = Visibility.Collapsed;
---
+++ CS3750_FinalProject/SplashScreen.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CS3750_FinalProject
+{
+    /// <summary>
+    /// Interaction logic for SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Open_File(object sender, RoutedEventArgs e)
+        {
+            var filedialog = new OpenFileDialog
+            {
+                // Only allows .csv files 
+                Filter = "CSV (*.csv)|*.csv"
+            };
+
+            if (filedialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string file = filedialog.FileName;
+                var mainWindow = new MainWindow(file);
+                mainWindow.Show();
+                this.Close();
+            }
+        }
+    }
+}
